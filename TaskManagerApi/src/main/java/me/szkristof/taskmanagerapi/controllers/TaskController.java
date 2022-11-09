eb28c0cc4de268a4bc0e3dc06efcbf71bf016cf6
Szabó Kristóf Gyula <szkristofgy@gmail.com>
2022-11-09 14:47:17 +0100

    Application properties are modified to change back an old path searching method (which is required to work with the current spring-boot version).
    TaskController is now modified to a proper version, and now it's working with Postman!

--- TaskManagerApi/src/main/java/me/szkristof/taskmanagerapi/controllers/TaskController.java
+++ TaskManagerApi/src/main/java/me/szkristof/taskmanagerapi/controllers/TaskController.java
@@ -8,26 +8,25 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/tasks")
 public class TaskController {
 
     private final List<TaskEntity> tasks = createTasks();
 
     private List<TaskEntity> createTasks() {
         List<TaskEntity> _tasks = new ArrayList<>();
-        _tasks.add(new TaskEntity(1, "Task 1", "Description 1", "Open", "High", Timestamp.valueOf("2022.11.09 12:00:00"), Timestamp.valueOf("2022.11.09 12:00:00")));
-        _tasks.add(new TaskEntity(2, "Task 2", "Description 2", "Open", "Low", Timestamp.valueOf("2022.11.09 12:00:00"), Timestamp.valueOf("2022.11.09 12:00:00")));
-        _tasks.add(new TaskEntity(3, "Task 3", "Description 3", "Closed", "Medium", Timestamp.valueOf("2022.11.09 12:00:00"), Timestamp.valueOf("2022.11.09 12:00:00")));
+        _tasks.add(new TaskEntity(1, "Task 1", "Description 1", "Open", "High", Timestamp.valueOf("2022-11-09 12:00:00"), Timestamp.valueOf("2022-11-09 12:00:00")));
+        _tasks.add(new TaskEntity(2, "Task 2", "Description 2", "Open", "Low", Timestamp.valueOf("2022-11-09 12:00:00"), Timestamp.valueOf("2022-11-09 12:00:00")));
+        _tasks.add(new TaskEntity(3, "Task 3", "Description 3", "Closed", "Medium", Timestamp.valueOf("2022-11-09 12:00:00"), Timestamp.valueOf("2022-11-09 12:00:00")));
 
         return _tasks;
     }
 
-    @RequestMapping(name = "/all", produces = "application/json", method = RequestMethod.GET)
+    @GetMapping("/tasks")
     public List<TaskEntity> getAllTasks() {
         return tasks;
     }
 
-    @RequestMapping(name = "/open", produces = "application/json", method = RequestMethod.GET)
+    @GetMapping("/tasks/opens")
     public List<TaskEntity> getOpenTasks() {
         List<TaskEntity> openTasks = new ArrayList<>();
         for (TaskEntity task : tasks) {
@@ -38,7 +37,7 @@ public class TaskController {
         return openTasks;
     }
 
-    @RequestMapping(name = "/closed", produces = "application/json", method = RequestMethod.GET)
+    @GetMapping("/tasks/closeds")
     public List<TaskEntity> getClosedTasks() {
         List<TaskEntity> closedTasks = new ArrayList<>();
         for (TaskEntity task : tasks) {
@@ -49,8 +48,8 @@ public class TaskController {
         return closedTasks;
     }
 
-    @RequestMapping(name = "/{id}", produces = "application/json", method = RequestMethod.GET)
-    public TaskEntity getTaskById(@RequestHeader int id) {
+    @GetMapping("/tasks/{id}")
+    public TaskEntity getTaskById(@PathVariable int id) {
         for (TaskEntity task : tasks) {
             if (task.getID() == id) {
                 return task;
@@ -59,8 +58,8 @@ public class TaskController {
         return null;
     }
 
-    @RequestMapping(name = "/{id}", produces = "application/json", method = RequestMethod.DELETE)
-    public TaskEntity deleteTaskById(@RequestHeader int id) {
+    @DeleteMapping("/tasks/delete/{id}")
+    public TaskEntity deleteTaskById(@PathVariable int id) {
         TaskEntity taskToDelete = null;
         for (TaskEntity task : tasks) {
             if (task.getID() == id) {
@@ -72,7 +71,7 @@ public class TaskController {
         return taskToDelete;
     }
 
-    @RequestMapping(name = "/create", produces = "application/json", method = RequestMethod.POST)
+    @PostMapping("/tasks/create")
     public TaskEntity createTask(
             @RequestBody TaskEntity task
     ) {
