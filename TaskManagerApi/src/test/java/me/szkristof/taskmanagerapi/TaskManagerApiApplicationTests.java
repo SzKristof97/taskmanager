8a1ddaf785311c12475d5a4f4324523a684552b1
Szabó Kristóf Gyula <szkristofgy@gmail.com>
2022-11-09 13:56:45 +0100

    New TaskManagerAPI

---
+++ TaskManagerApi/src/test/java/me/szkristof/taskmanagerapi/TaskManagerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package me.szkristof.taskmanagerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
