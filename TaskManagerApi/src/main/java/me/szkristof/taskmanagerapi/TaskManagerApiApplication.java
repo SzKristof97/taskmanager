9076e7b99ad80ba2a113de6000d786395cb35cb4
Szabó Kristóf Gyula <szkristofgy@gmail.com>
2022-11-09 14:05:54 +0100

    Adding and enabling the Swagger2

--- TaskManagerApi/src/main/java/me/szkristof/taskmanagerapi/TaskManagerApiApplication.java
+++ TaskManagerApi/src/main/java/me/szkristof/taskmanagerapi/TaskManagerApiApplication.java
@@ -2,8 +2,10 @@ package me.szkristof.taskmanagerapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class TaskManagerApiApplication {
 
     public static void main(String[] args) {
